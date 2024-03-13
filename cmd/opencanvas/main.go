9ae1a0eaedc14e03dd84d3ace1b8c189a3b48837
Wojciech Kwolek <wojciech@kwolek.io>
2024-03-13 04:54:56 +0100

    renderer stub

--- cmd/opencanvas/main.go
+++ cmd/opencanvas/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/irth/opencanvas/pkg/opencanvas"
+	opencanvasRender "github.com/irth/opencanvas/pkg/renderer"
+)
 
 func main() {
-	fmt.Println("Hi!")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: opencanvas <input file> <output file>")
+		os.Exit(1)
+	}
+
+	inFileName := os.Args[1]
+	outFileName := os.Args[2]
+
+	inFile, err := os.Open(inFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer inFile.Close()
+
+	outFile, err := os.Create(outFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+
+	var canvas opencanvas.Canvas
+	err = json.NewDecoder(inFile).Decode(&canvas)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = opencanvasRender.Render(outFile, &canvas)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
---
+++ pkg/renderer/render.go
@@ -0,0 +1,17 @@
+package opencanvasRender
+
+import (
+	_ "embed"
+	"html/template"
+	"io"
+
+	"github.com/irth/opencanvas/pkg/opencanvas"
+)
+
+//go:embed template.html
+var templateString string
+var tmpl *template.Template = template.Must(template.New("").Parse(templateString))
+
+func Render(w io.Writer, canvas *opencanvas.Canvas) error {
+	return tmpl.Execute(w, canvas)
+}
